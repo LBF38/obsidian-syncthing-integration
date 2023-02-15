8069df8af9ae26e2eb90d470ba142cee84ed470f
LBF38 <contact.lbf38@gmail.com>
2023-02-15 17:31:45 +0100

    feat(sync algorithm): :sparkles: Add the comparison of hashes for file integrity
    The method calculates each file hash and compare them to return a boolean. It helps for the sync algorithm decisions

--- src/sync_algorithm.ts
+++ src/sync_algorithm.ts
@@ -1,14 +1,24 @@
 import { TFile } from "obsidian";
+import { createHash } from "crypto";
 
 export enum syncChoice {
 	upload,
 	download,
 	ignore,
+	delete,
 }
 
 export interface syncFile {
 	localToRemote: syncChoice.upload | syncChoice.ignore;
 	remoteToLocal: syncChoice.download | syncChoice.ignore;
+}
+
+export interface fileChoice {
+	localFileChoice: syncChoice.upload | syncChoice.ignore | syncChoice.delete;
+	remoteFileChoice:
+		| syncChoice.download
+		| syncChoice.ignore
+		| syncChoice.delete;
 }
 
 export class syncAlgorithm {
@@ -19,14 +29,35 @@ export class syncAlgorithm {
 		remoteToLocal: syncChoice.ignore,
 	};
 
-	compareFiles(localFile: TFile, remoteFile: TFile): syncFile {
-		if (localFile.stat.mtime > remoteFile.stat.mtime) {
-			this.syncFiles.localToRemote = syncChoice.upload;
-			this.syncFiles.remoteToLocal = syncChoice.ignore;
-			return this.syncFiles;
+	compareFile(localVersion: TFile, remoteVersion: TFile): fileChoice {
+		const fileChoice: fileChoice = {
+			localFileChoice: syncChoice.ignore,
+			remoteFileChoice: syncChoice.ignore,
+		};
+
+		if (localVersion.stat.mtime > remoteVersion.stat.mtime) {
+			fileChoice.localFileChoice = syncChoice.upload;
+			fileChoice.remoteFileChoice = syncChoice.ignore;
+			return fileChoice;
+		} else if (localVersion.stat.mtime < remoteVersion.stat.mtime) {
+			fileChoice.localFileChoice = syncChoice.ignore;
+			fileChoice.remoteFileChoice = syncChoice.download;
+			return fileChoice;
 		}
-		this.syncFiles.localToRemote = syncChoice.ignore;
-		this.syncFiles.remoteToLocal = syncChoice.download;
-		return this.syncFiles;
+		return fileChoice;
+	}
+
+	isSameContent(
+		localFileContent: string,
+		remoteFileContent: string,
+		hashAlgorithm: "sha1" | "sha256" = "sha1"
+	): boolean {
+		const localHash = createHash(hashAlgorithm)
+			.update(localFileContent)
+			.digest("hex");
+		const remoteHash = createHash(hashAlgorithm)
+			.update(remoteFileContent)
+			.digest("hex");
+		return localHash === remoteHash;
 	}
 }
--- tests/sync_algorithm.test.ts
+++ tests/sync_algorithm.test.ts
@@ -14,20 +14,78 @@ jest.mock("obsidian", () => {
 });
 
 describe("Sync algorithm", () => {
-	beforeEach(() => {});
-	it("should return localToRemote when the local file is newer than the remote file.", () => {
+	let localFile: TFile;
+	let remoteFile: TFile;
+	let sync_algorithm: syncAlgorithm;
+
+	beforeEach(() => {
+		localFile = new TFile();
+		remoteFile = new TFile();
+		sync_algorithm = new syncAlgorithm();
+	});
+
+	it("should return upload/ignore when the local file is newer than the remote file.", () => {
 		// arrange
-		const localFile: TFile = new TFile();
-		const remoteFile: TFile = new TFile();
 		localFile.stat.mtime = 100;
 		remoteFile.stat.mtime = 50;
-		const sync_algorithm = new syncAlgorithm();
 
 		// act
-		const choice = sync_algorithm.compareFiles(localFile, remoteFile);
+		const choice = sync_algorithm.compareFile(localFile, remoteFile);
 
 		// assert
-		expect(choice.localToRemote).toBe(syncChoice.upload);
-		expect(choice.remoteToLocal).toBe(syncChoice.ignore);
+		expect(choice.localFileChoice).toBe(syncChoice.upload);
+		expect(choice.remoteFileChoice).toBe(syncChoice.ignore);
+	});
+
+	it("should return ignore/download when the local file is newer than the remote file.", () => {
+		// arrange
+		localFile.stat.mtime = 50;
+		remoteFile.stat.mtime = 100;
+
+		// act
+		const choice = sync_algorithm.compareFile(localFile, remoteFile);
+
+		// assert
+		expect(choice.localFileChoice).toBe(syncChoice.ignore);
+		expect(choice.remoteFileChoice).toBe(syncChoice.download);
+	});
+
+	it("should return ignore/ignore when the local file is the same age as the remote file.", () => {
+		// arrange
+		localFile.stat.mtime = 100;
+		remoteFile.stat.mtime = 100;
+
+		// act
+		const choice = sync_algorithm.compareFile(localFile, remoteFile);
+
+		// assert
+		expect(choice.localFileChoice).toBe(syncChoice.ignore);
+		expect(choice.remoteFileChoice).toBe(syncChoice.ignore);
+	});
+
+	it("should return True as the content of the files is the same.", () => {
+		// Arrange
+		const localFileContent = "This is a test file";
+		const remoteFileContent = "This is a test file";
+		// Act
+		const isSameContent = sync_algorithm.isSameContent(
+			localFileContent,
+			remoteFileContent
+		);
+		// Assert
+		expect(isSameContent).toBe(true);
+	});
+
+	it("should return False as the content of the files is different.", () => {
+		// Arrange
+		const localFileContent = "This is a test file";
+		const remoteFileContent = "This is a different test file";
+		// Act
+		const isSameContent = sync_algorithm.isSameContent(
+			localFileContent,
+			remoteFileContent
+		);
+		// Assert
+		expect(isSameContent).toBe(false);
 	});
 });
