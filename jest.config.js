2e029ea7ab88d4a8145ca03d03176d34a534d9e0
LBF38 <contact.lbf38@gmail.com>
2023-02-15 15:00:09 +0100

    build(project): :bug: Fixed the Jest config for making unit tests with Obsidian module
    The issue is that obsidian doesn't give a proper module to interact with.
    It only gives types defs
    so we have to mock everything when testing with Jest

--- jest.config.js
+++ jest.config.js
@@ -2,12 +2,12 @@
 module.exports = {
 	preset: "ts-jest",
 	testEnvironment: "node",
-	roots: ["<rootDir>"],
-	modulePaths: ["<rootDir>"],
-	// moduleNameMapper: {
-	// 	obsidian: "<rootDir>/node_modules/obsidian",
-	// },
-	transform: {
-		"^.+\\.ts$": "ts-jest",
+	// workaround because obsidian api does not have transpiled js included in its node module
+	// From: https://github.com/kulshekhar/ts-jest/issues/1523
+	// forcefully map obsidian to its .d.ts file
+	moduleNameMapper: {
+		obsidian: "<rootDir>/node_modules/obsidian/obsidian.d.ts",
 	},
+	// ignore patterns for obsidian or you will get import/export errors within obsidian
+	transformIgnorePatterns: ["node_modules/(?!obsidian/.*)"],
 };
