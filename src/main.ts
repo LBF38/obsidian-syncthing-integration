c4350a67475596b550988c94d0731b3b0388a874
LBF38 <contact.lbf38@gmail.com>
2023-05-14 19:25:13 +0200

    feat(main): :construction: lab code gen w/ chatgpt
    trying to use chatgpt to generate the code for making the UI.
    (for the files diff).

--- src/main.ts
+++ src/main.ts
@@ -1,340 +1,126 @@
-import {
-	App,
-	Editor,
-	ItemView,
-	MarkdownFileInfo,
-	MarkdownView,
-	Modal,
-	Notice,
-	Plugin,
-	PluginSettingTab,
-	Setting,
-	TAbstractFile,
-	TFile,
-	WorkspaceLeaf,
-} from "obsidian";
 import { createTwoFilesPatch } from "diff";
-import { defaultDiff2HtmlConfig, html } from "diff2html";
+import { html } from "diff2html";
+import { App, Modal, Plugin, TFile } from "obsidian";
 
-//! Remember to rename these classes and interfaces!
-
-interface MyPluginSettings {
-	mySetting: string;
-	myToggleSetting: boolean;
-	excludedFolders: boolean[];
-}
-
-const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: "default",
-	myToggleSetting: false,
-	excludedFolders: [false],
-};
-
-export default class MyPlugin extends Plugin {
-	settings: MyPluginSettings = DEFAULT_SETTINGS;
-
+export default class SyncThingPlugin extends Plugin {
 	async onload() {
-		await this.loadSettings();
+		console.log("SyncThing plugin loaded!");
 
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon(
-			"cloud",
-			"OneDrive Sync",
-			(evt: MouseEvent) => {
-				// Called when the user clicks the icon.
-				new Notice("OneDrive Sync is not yet implemented.");
-			}
-		);
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass("my-plugin-ribbon-class");
-
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText("OneDrive Sync status");
-		statusBarItemEl.onClickEvent(() => {
-			new Notice("OneDrive Sync is not yet implemented.");
-		});
-
-		// This adds a simple command that can be triggered anywhere
+		// Register a command to show the list of conflicting files
 		this.addCommand({
-			id: "open-onedrive-sync-modal",
-			name: "Open OneDrive Sync modal",
+			id: "show-conflicting-files",
+			name: "Show Conflicting Files",
 			callback: () => {
-				new SampleModal(this.app).open();
-			},
-		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: "sample-editor-command",
-			name: "Sample editor command",
-			editorCallback: (
-				editor: Editor,
-				ctx: MarkdownView | MarkdownFileInfo
-			) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection("Sample Editor Command");
-			},
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: "open-sample-modal-complex",
-			name: "Open sample modal (complex)",
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView =
-					this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
-
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
-				}
+				this.showConflictingFiles();
 			},
 		});
 
-		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this));
-
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, "click", (evt: MouseEvent) => {
-			console.log("click", evt);
+		this.addRibbonIcon("wrench", "Show Conflict Files", () => {
+			new ConflictFilesModal(this.app).open();
 		});
 
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(
-			window.setInterval(() => console.log("setInterval"), 5 * 60 * 1000)
-		);
-
-		// Example of a custom view
-		this.registerView(VIEW_TYPE_EXAMPLE, (leaf) => new ExampleView(leaf));
-
-		this.addRibbonIcon("dice", "Activate view", () => {
-			const file = this.app.vault.getFiles()[0];
-			this.activateView(file);
+		// Add a toolbar icon to show the list of conflicting files
+		this.addRibbonIcon("cloud", "Show Conflicting Files", () => {
+			this.showConflictingFiles();
 		});
 	}
 
-	onunload() {
-		this.app.workspace.detachLeavesOfType(VIEW_TYPE_EXAMPLE);
-	}
+	async showConflictingFiles() {
+		// Get the list of conflicting files from your SyncThing API
+		const conflictingFiles: TFile[] = this.app.vault
+			.getFiles()
+			.filter((file) => {
+				return file;
+			});
 
-	async loadSettings() {
-		this.settings = Object.assign(
-			{},
-			DEFAULT_SETTINGS,
-			await this.loadData()
-		);
-	}
+		// Create a new modal to show the conflicting files
+		const modal = new Modal(this.app);
+		modal.titleEl.setText("Conflicting Files");
 
-	async saveSettings() {
-		await this.saveData(this.settings);
-	}
+		// Create a new list element to show the conflicting files
+		const listEl = modal.contentEl.createEl("ul");
+		listEl.classList.add("sync-thing-conflicting-files");
 
-	async activateView(file: TFile) {
-		this.app.workspace.detachLeavesOfType(VIEW_TYPE_EXAMPLE);
+		// Add each conflicting file to the list
+		for (const file of conflictingFiles) {
+			const listItemEl = modal.contentEl.createEl("li");
+			listItemEl.classList.add("sync-thing-conflicting-file");
 
-		await this.app.workspace.getLeaf("split").setViewState({
-			type: VIEW_TYPE_EXAMPLE,
-			state: this.app.vault.read(file),
-			active: true,
-		});
+			const linkEl = modal.contentEl.createEl("a");
+			linkEl.innerText = file.basename;
+			linkEl.href = `#${file.path}`;
 
-		this.app.workspace.revealLeaf(
-			this.app.workspace.getLeavesOfType(VIEW_TYPE_EXAMPLE)[0]
-		);
+			// When the user clicks on a link, show the conflicting file in a new tab
+			linkEl.onclick = (event) => {
+				event.preventDefault();
+				this.app.workspace.openLinkText(file.path, "", true, {
+					active: true,
+				});
+			};
+
+			listItemEl.appendChild(linkEl);
+			listEl.appendChild(listItemEl);
+		}
+
+		modal.open();
 	}
 }
 
-class SampleModal extends Modal {
-	text2display: string;
-	constructor(app: App, text2display?: string) {
+class ConflictFilesModal extends Modal {
+	constructor(app: App) {
 		super(app);
-		this.text2display = text2display ? text2display : "";
 	}
 
 	onOpen() {
 		const { contentEl } = this;
-		contentEl.setText("Welcome to OneDrive Sync!");
-		contentEl.innerHTML += this.text2display;
+
+		contentEl.createEl("h2", { text: "Conflict Files" }).addClass("modal-title");
+		contentEl.addClass("conflict-view");
+		// Second column with diff of selected file
+		const diffView = contentEl.createEl("div", { cls: "diff-view" });
+
+		// Third column with full content of selected file
+		const fileView = contentEl.createEl("div", { cls: "file-view" });
+
+		// First column with list of files
+		const filesList = contentEl.createEl("div", { cls: "files-list" });
+		const originalFile = this.app.vault.getFiles()[0];
+		const files = this.app.vault.getFiles().slice(1, -1);
+		for (const file of files) {
+			const fileEl = filesList.createEl("div", { text: file.name });
+			fileEl.addEventListener("click", () => {
+				this.showFileDiff(originalFile, file, diffView, fileView);
+			});
+		}
+	}
+
+	private async showFileDiff(
+		originalFile: TFile,
+		conflictingFile: TFile,
+		diffView: HTMLElement,
+		fileView: HTMLElement
+	) {
+		// Load the two versions of the file
+		const originalContent = await app.vault.read(originalFile);
+		const conflictingContent = await app.vault.read(conflictingFile);
+
+		// Compute the diff between the two files
+		const diff = createTwoFilesPatch(
+			originalFile.basename,
+			conflictingFile.basename,
+			originalContent,
+			conflictingContent
+		);
+
+		// Display the diff in the second column
+		diffView.innerHTML = html(diff);
+
+		// Display the full content of the file in the third column
+		fileView.setText(originalContent);
 	}
 
 	onClose() {
 		const { contentEl } = this;
 		contentEl.empty();
-	}
-}
-
-class SampleSettingTab extends PluginSettingTab {
-	plugin: MyPlugin;
-
-	constructor(app: App, plugin: MyPlugin) {
-		super(app, plugin);
-		this.plugin = plugin;
-	}
-
-	display(): void {
-		const { containerEl } = this;
-
-		containerEl.empty();
-
-		containerEl.createEl("h2", { text: "Settings for my awesome plugin." });
-
-		new Setting(containerEl)
-			.setName("Setting #1")
-			.setDesc("It's a secret")
-			.addText((text) =>
-				text
-					.setPlaceholder("Enter your secret")
-					.setValue(this.plugin.settings.mySetting)
-					.onChange(async (value) => {
-						console.log("Secret: " + value);
-						this.plugin.settings.mySetting = value;
-						await this.plugin.saveSettings();
-					})
-			);
-
-		containerEl.createEl("h3", { text: "SyncThing" });
-		new Setting(containerEl)
-			.setName("Connecting to SyncThing")
-			.addToggle((toggle) =>
-				toggle
-					.setValue(this.plugin.settings.myToggleSetting)
-					.onChange(async (value) => {
-						console.log("Toggle: " + value);
-						this.plugin.settings.myToggleSetting = value;
-						await this.plugin.saveSettings();
-					})
-			);
-		new Setting(containerEl)
-			.setName("Exclude files")
-			.setDesc("Files to exclude from sync")
-			.addButton((button) =>
-				button.setButtonText("Add").onClick(() => {
-					new Notice("Button pressed");
-					console.log("Button pressed");
-				})
-			);
-		const all_files: TAbstractFile[] = this.app.vault.getAllLoadedFiles();
-		for (let k = 0; k < all_files.length; k++) {
-			const abstract_files = all_files[k];
-			if (abstract_files.name === "") continue;
-			const input = containerEl.createEl("input", {
-				type: "checkbox",
-				attr: { id: abstract_files.name, name: abstract_files.name },
-				title: abstract_files.name,
-			});
-			containerEl
-				.createEl("label", {
-					text: abstract_files.name,
-					value: abstract_files.name,
-					attr: { for: abstract_files.name },
-				})
-				.appendChild(containerEl.createEl("br"));
-			if (abstract_files instanceof TFile) {
-				input.style.textIndent = "50px";
-			}
-		}
-	}
-}
-
-export const VIEW_TYPE_EXAMPLE = "example-view";
-
-export class ExampleView extends ItemView {
-	constructor(leaf: WorkspaceLeaf) {
-		super(leaf);
-	}
-
-	getViewType() {
-		return VIEW_TYPE_EXAMPLE;
-	}
-
-	getDisplayText() {
-		return "Example view";
-	}
-
-	async onOpen() {
-		const container = this.containerEl.children[1];
-		container.empty();
-		container.createEl("h4", { text: "Example view" });
-		this.contentEl.setText("Hello world!");
-		this.contentEl.createEl("h1", { text: "Hello world!" });
-		const file1 = {
-			tfile: this.app.vault.getMarkdownFiles()[0],
-			content: await this.app.vault.read(
-				this.app.vault.getMarkdownFiles()[0]
-			),
-		};
-		const file2 = {
-			tfile: this.app.vault.getMarkdownFiles()[1],
-			content: await this.app.vault.read(
-				this.app.vault.getMarkdownFiles()[1]
-			),
-		};
-		this.contentEl.createEl("h1", { text: file1.tfile.basename });
-		this.contentEl.createDiv({ text: file1.content });
-		this.contentEl.createEl("h1", { text: file2.tfile.basename });
-		this.contentEl.createDiv({ text: file2.content });
-		this.containerEl
-			.createEl("button", { text: "Diff" })
-			.addEventListener("click", () => {
-				createTwoFilesPatch(
-					file1.tfile.basename,
-					file2.tfile.basename,
-					file1.content,
-					file2.content
-				);
-			});
-		const difference = createTwoFilesPatch(
-			file1.tfile.basename,
-			file2.tfile.basename,
-			file1.content,
-			file2.content
-		);
-		this.contentEl.createEl("h1", { text: "Difference" });
-		this.contentEl.createDiv({ text: difference });
-		this.contentEl
-			.createEl("button", { text: "Open diff between files" })
-			.addEventListener("click", () => {
-				new SampleModal(app, html(difference, defaultDiff2HtmlConfig)).open();
-			});
-	}
-
-	async onClose() {
-		// Nothing to clean up.
-	}
-}
-
-export class ExampleMarkdownView extends MarkdownView {
-	constructor(leaf: WorkspaceLeaf) {
-		super(leaf);
-	}
-
-	getViewType() {
-		return VIEW_TYPE_EXAMPLE;
-	}
-
-	getDisplayText() {
-		return "Example markdown view";
-	}
-
-	async onOpen() {
-		const file = this.app.vault.getMarkdownFiles()[0];
-		this.contentEl.setText(await this.app.vault.read(file));
-		this.leaf
-			.getContainer()
-			.doc.createElement("h4")
-			.setText("Example markdown view");
-		// const container = this.containerEl.children[1];
-		// container.empty();
-		// container.createEl("h4", { text: "Example markdown view" });
-	}
-
-	async onClose() {
-		// Nothing to clean up.
 	}
 }
