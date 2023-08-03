3887d9042d9f21b6032b4221463fbf3e67a5d812
LBF38 <contact.lbf38@gmail.com>
2023-08-03 18:18:58 +0200

    ci(gh actions): :green_heart: update CI
    update prepare CI script for release (semantic-release)
    
    Fixes #85

---
+++ scripts/prepareRelease.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+
+if ! (git log -1 --pretty=%B | grep -q "chore(release): :bookmark: $1"); then
+	pnpm run version
+	git add manifest.json versions.json package.json docs/CHANGELOG.md
+	# if [ "$(git branch --list 'release/*' | wc -l)" -gt 2 ]; then
+	# 	echo "multiple release branches exist"
+	# 	git
+	# fi
+	git checkout -b release/"$1"
+	git commit -m "chore(release): :bookmark: $1 [skip ci]\n\n$2"
+	git push --set-upstream origin release/"$1" --force
+	if (gh pr list | grep -q "release/$1"); then
+		echo "a release PR already exists"
+		pr_number=$(gh pr list --state open | grep "release/$1" | awk '{print $1}')
+		gh pr edit "$pr_number" --title "chore(release): :bookmark: $1" --body "$2"
+	else
+		echo "creating the release PR"
+		gh pr create --title "chore(release): :bookmark: $1" --body "$2" --base master --head release/"$1"
+	fi
+	git checkout master
+	echo "Need to merge the release PR"
+	exit 0
+fi
+
+echo "creating and publishing the release $1 to GitHub"
