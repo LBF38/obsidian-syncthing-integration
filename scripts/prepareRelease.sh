61c34fb00d770e4fd41855c60af1a960b94fcce5
LBF38 <contact.lbf38@gmail.com>
2023-08-03 18:46:11 +0200

    fix(gh actions): :green_heart: run script on bash

--- scripts/prepareRelease.sh
+++ scripts/prepareRelease.sh
@@ -1,16 +1,12 @@
-#!/bin/sh
+#!/bin/bash
 
-if ! (git log -1 --pretty=%B | grep -q "chore(release): :bookmark: $1"); then
+if ! git log -1 --pretty=%B | grep -q "chore(release): :bookmark: $1"; then
 	pnpm run version
 	git add manifest.json versions.json package.json docs/CHANGELOG.md
-	# if [ "$(git branch --list 'release/*' | wc -l)" -gt 2 ]; then
-	# 	echo "multiple release branches exist"
-	# 	git
-	# fi
 	git checkout -b release/"$1"
 	git commit -m "chore(release): :bookmark: $1 [skip ci]\n\n$2"
 	git push --set-upstream origin release/"$1" --force
-	if (gh pr list | grep -q "release/$1"); then
+	if gh pr list | grep -q "release/$1"; then
 		echo "a release PR already exists"
 		pr_number=$(gh pr list --state open | grep "release/$1" | awk '{print $1}')
 		gh pr edit "$pr_number" --title "chore(release): :bookmark: $1" --body "$2"
@@ -20,7 +16,7 @@ if ! (git log -1 --pretty=%B | grep -q "chore(release): :bookmark: $1"); then
 	fi
 	git checkout master
 	echo "Need to merge the release PR"
-	exit 0
+	exit 1
 fi
 
 echo "creating and publishing the release $1 to GitHub"
