39cd7ce801a905844c6eb0848ef9ba5e1a514cd3
LBF38 <contact.lbf38@gmail.com>
2023-12-07 13:42:29 +0100

    feat(ui): :sparkles: big update status bar
    add new features to the status bar
    also refactor code to use the new method from main controller
    ready to be released.
    Warning: still have some issues w/ renaming conflict to original file

--- src/controllers/status_bar.ts
+++ src/controllers/status_bar.ts
@@ -7,6 +7,7 @@ export class SyncthingStatusBar {
 	app: App;
 	activeConflicts?: TFile[];
 	currentFile: TFile | null;
+	originalFile?: TFile;
 	constructor(public status_bar: HTMLElement, public plugin: SyncthingPlugin) {
 		this.app = plugin.app;
 		this.currentFile = this.app.workspace.getActiveFile();
@@ -31,27 +32,62 @@ export class SyncthingStatusBar {
 				});
 				item.setIcon("arrow-up-right");
 			})
-			if (this.currentFile && isConflictFilename(this.currentFile.basename)) {
+			if (this.currentFile && isConflictFilename(this.currentFile.name) && this.originalFile === undefined) {
+				const file = this.currentFile;
 				menu.addItem((item) => {
-					item.setTitle("Rename to original").onClick(() => {
-						new Notice("Not implemented yet !");
-						// TODO: add this feature.
-						// TODO: get original file if it exists => see getDiffFiles in main_controller.ts
+					item.setTitle("Rename to original").onClick(async () => {
+						const conflict = parseConflictFilename(file.name);
+						if (conflict instanceof Failure) {
+							new Notice("Failed to parse conflict filename");
+							console.error(conflict.message);
+							return;
+						}
+						new Notice("Not implemented yet");
+						// if (await this.app.vault.adapter.exists(normalizePath(file.name))) {
+						// 	new Notice("File with original name already exists");
+						// 	await this.app.vault.delete(this.app.vault.getAbstractFileByPath(normalizePath(file.name)) as TFile, true);
+						// 	new Notice("Deleted original file");
+						// 	// return;
+						// }
+						// await this.app.vault.adapter.rename(normalizePath(`${conflict.filename}.${conflict.extension}`), normalizePath(file.name));
+						// new Notice("Renamed conflict to original");
 					})
 					item.setIcon("pencil");
 				})
 			}
+			if (this.originalFile !== undefined) {
+				menu.addItem((item) => {
+					item.setTitle("Open original file").onClick(async () => await this.app.workspace
+						.createLeafBySplit(this.app.workspace.getLeaf())
+						.openFile(this.originalFile as TFile))
+					item.setIcon("arrow-up-right");
+				});
+			}
 			menu.addSeparator();
+			this.activeConflicts?.sort((a, b) => {
+				const aProps = parseConflictFilename(a.basename);
+				const bProps = parseConflictFilename(b.basename);
+				if (aProps instanceof Failure || bProps instanceof Failure) {
+					return 0;
+				}
+				if (aProps.dateTime > bProps.dateTime) {
+					return -1;
+				}
+				if (aProps.dateTime < bProps.dateTime) {
+					return 1;
+				}
+				return 0;
+			})
 			this.activeConflicts?.forEach((file) => {
 				menu.addItem((item) => {
-					// if (isConflictFilename(file.basename)) {
-					// 	const conflict = parseConflictFilename(file.basename);
-					// 	if (conflict instanceof Failure) {
-					// 		return;
-					// 	}
-					// 	item.setTitle(`${conflict.dateTime.toISOString()} ${conflict.filename} - ${conflict.modifiedBy}`);
-					// }
-					item.setTitle(file.basename);
+					if (isConflictFilename(file.basename)) {
+						const conflict = parseConflictFilename(file.basename);
+						if (conflict instanceof Failure) {
+							return;
+						}
+						item.setTitle(`${conflict.filename} (caused by ${conflict.modifiedBy} on ${conflict.dateTime.toISOString().replace("T", " ").replace("Z", "")})`);
+					}
+					// item.setTitle(file.basename);
 					item.setIcon("file");
 					item.onClick(async () => {
 						await this.app.workspace
@@ -59,7 +95,7 @@ export class SyncthingStatusBar {
 							.openFile(file);
 					});
 				});
-			})
+			});
 
 			menu.showAtPosition({
 				x: window.innerWidth - 15, y: window.innerHeight - 37,
@@ -77,22 +113,12 @@ export class SyncthingStatusBar {
 			this.status_bar.empty();
 			return;
 		}
-		this.plugin.syncthingController.getConflicts().then((conflicts) => {
-			if (conflicts instanceof Failure) {
-				console.error(conflicts); // TODO: refactor w/ logging framework
-				return;
-			}
-			if (isConflictFilename(activeFile.basename)) {
-				const conflictFilename = parseConflictFilename(activeFile.basename);
-				if (conflictFilename instanceof Failure) {
-					console.error(conflictFilename.message); // TODO: refactor w/ logging framework
-					return;
-				}
-				this.activeConflicts = conflicts.get(conflictFilename.filename);
-			} else {
-				this.activeConflicts = conflicts.get(activeFile.basename);
-			}
-			if (!this.activeConflicts) {
+		// Get all conflicting files + original file
+		this.plugin.syncthingController.getConflictsWithOriginal(activeFile).then((result) => {
+			this.activeConflicts = result.conflicts;
+			this.activeConflicts.remove(activeFile);
+			this.originalFile = result.originalFile;
+			if (!this.activeConflicts || this.activeConflicts.length === 0) {
 				this.status_bar.empty();
 				return;
 			}
