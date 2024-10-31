f4e7ed83d04061f6c1c6ac4a1e5854814f3b5f77
LBF38 <contact.lbf38@gmail.com>
2024-10-31 22:01:26 +0100

    refactor(dev): :construction: testing xstate machines for orchestrating everything
    to see if it can work w/ that

--- src/components/machines.ts
+++ src/components/machines.ts
@@ -1,7 +1,7 @@
 import { SyncthingFromREST } from 'src/data/syncthing_remote_datasource';
 import { SyncthingConfiguration, SyncthingSystemStatus } from 'src/models/entities';
 import { Output } from 'valibot';
-import { assign, fromPromise, setup } from 'xstate';
+import { assign, fromPromise, log, setup } from 'xstate';
 
 const fetchConfiguration = fromPromise(async ({ input }: { input: { syncthingREST: SyncthingFromREST } }) => {
 	return await input.syncthingREST.config.all();
@@ -12,75 +12,70 @@ const fetchSystemStatus = fromPromise(async ({ input }: { input: { syncthingREST
 });
 
 
-export const syncthingConfigurationMachine = setup({
-	types: {
-		context: {} as {
-			configuration?: Output<typeof SyncthingConfiguration>;
-			syncthingREST: SyncthingFromREST;
-			error?: string;
-		},
-		input: {} as {
-			syncthingREST: SyncthingFromREST;
-		},
-	},
-}).createMachine({
-	/** @xstate-layout N4IgpgJg5mDOIC5QAoC2BDAxgCwJYDswBKAOlwgBswBiAD1gBd0GwT0AzFgJ2QEYAGQUWposeQqXJUA2vwC6iUAAcA9rFwNcK-IpC1EvAGyGSAJkEXD-AMwAWY7wCsAGhABPRKdMnbjwbwCADgBOK0NbAHYAXyjXURwCYhJ2MAYE-CgAAkxtdlwoAFcuZi18aghtVkZmVnjxJJS08Syc-DzC4s1tWQUkEFV1Lp0+-QRHa2sSSNtgiL9rR0D+XlcPBC8fP2Wg0P5w6NiQOsTSAqUIGuzc-KKS7WomLhgGHt0BjVLdUcdbXjNrCK2QK+cJ2CIrdyIQJ-awWAKmRwRQKmcGmGKHfAqCBwXTHCRvNQfbRfRAAWkMqzJvAiJHCwXphh+QN4S14MTiGHSSSkYAJg0+I0QtlMlIQgRMwUCE1sdhswRZy3ZR059VIjXSLWuHTuw2UhKGJPW4pIwPps2svGF5n4Lkh62WtK2vC88uF-FMtiVeKSZwuLCubRunQFev5xMFCH2JAmgNChnlwJlopCUyldll1gTisO3tIhAA7plqv7eHyibq9EL+MF-oDgY5QZFLaL447-C7LeYPeiokA */
-	id: "syncthingConfiguration",
-	context: ({ input }) => ({
-		syncthingREST: input.syncthingREST,
-		configuration: undefined,
-	}),
-	initial: "idle",
-	states: {
-		idle: {
-			after: {
-				1000: "fetching configuration",
-			}
-		},
-		"fetching configuration": {
-			invoke: {
-				id: "fetchConfig",
-				src: fetchConfiguration,
-				input: ({ context: { syncthingREST } }) => ({ syncthingREST }),
-				onDone: {
-					target: "success",
-					actions: assign({
-						configuration: ({ event }) => {
-							return event.output
-						}
-					})
-				},
-				onError: {
-					target: "failure",
-					actions: assign({
-						error: () => {
-							return "An error occured"
-						}
-					})
-				}
-			},
-		},
-		success: {
-			on: {
-				FETCH: "idle"
-			}
-		},
-		failure: {
-			on: {
-				RETRY: "fetching configuration"
-			}
-		}
-	},
-
-});
-
-export const syncthingControllerMachine = setup({
+const updateSyncthingMachine = setup({
 	types: {
 		context: {} as {
 			system_status?: Output<typeof SyncthingSystemStatus>;
 			configuration?: Output<typeof SyncthingConfiguration>;
 			syncthingREST: SyncthingFromREST;
-			error?: string[];
+		},
+		input: {} as {
+			syncthingREST: SyncthingFromREST;
+		}
+	}
+}).createMachine({
+	id: "updateSyncthing",
+	context: ({ input }) => ({
+		system_status: undefined,
+		configuration: undefined,
+		syncthingREST: input.syncthingREST,
+	}),
+	initial: "update data",
+	states: {
+		"update data": {
+			invoke: [
+				{
+					src: fetchConfiguration,
+					input: ({ context: { syncthingREST } }) => ({ syncthingREST }),
+					onDone: {
+						actions: assign({
+							configuration: ({ event }) => {
+								return event.output
+							}
+						}),
+					},
+				},
+				{
+					src: fetchSystemStatus,
+					input: ({ context: { syncthingREST } }) => ({ syncthingREST }),
+					onDone: {
+						actions: assign({
+							system_status: ({ event }) => {
+								return event.output
+							}
+						})
+					}
+				}
+			],
+			always: {
+				guard: ({ context: { system_status, configuration } }) => {
+					return !!(system_status && configuration)
+				},
+				target: "success"
+			}
+		},
+		success: {
+			type: "final",
+			output: ({ context }) => context
+		}
+	}
+});
+
+// useful example here: https://github.com/statelyai/xstate/blob/main/examples/workflow-car-vitals/main.ts
+export const syncthingControllerMachine = setup({
+	types: {
+		context: {} as {
+			syncthingREST: SyncthingFromREST;
 		},
 		input: {} as {
 			syncthingREST: SyncthingFromREST;
@@ -95,145 +90,46 @@ export const syncthingControllerMachine = setup({
 	initial: "idle",
 	states: {
 		idle: {
-			after: {
-				1000: "update syncthing",
+			on: {
+				StartUpdateSyncthing: "update syncthing",
 			},
 		},
 		"update syncthing": {
 			invoke: [
 				{
-					src: fetchConfiguration,
+					src: updateSyncthingMachine,
 					input: ({ context: { syncthingREST } }) => ({ syncthingREST }),
 					onDone: {
-						actions: assign({
-							configuration: ({ event }) => {
-								return event.output
-							}
-						}),
+						actions: ({ event }) => {
+							console.log(event.output)
+						},
+						target: "WaitUntilNextUpdate"
 					},
-					onError: {
-						target: "failure",
-						actions: assign({
-							error: ({ event, context }) => {
-								return context.error?.concat(`${event.type} ${event.error}`) ?? [`${event.type} ${event.error}`]
-							}
-						})
-					}
-				},
-				{
-					src: fetchSystemStatus,
-					input: ({ context: { syncthingREST } }) => ({
-						syncthingREST
-					}),
-					onDone: {
-						actions: assign({
-							system_status: ({ event }) => {
-								return event.output
-							}
-						})
-					},
-					onError: {
-						target: "failure",
-						actions: assign({
-							error: ({ event, context }) => {
-								return context.error?.concat(`${event.type} ${event.error}`) ?? [`${event.type} ${event.error}`]
-							}
-						})
-					}
 				},
 			],
-			onDone: {
-				guard: ({ context: { error } }) => error?.length === 0 || error === undefined,
-				target: "success"
-			},
+			// onDone: {
+			// 	guard: ({ context: { error } }) => error?.length === 0 || error === undefined,
+			// 	target: "success"
+			// },
 		},
-		success: {
-			type: "final",
-		},
-		failure: {
-			on: {
-				RETRY: "update syncthing"
-			}
-		}
-	}
-});
-
-
-export const syncthingSystemStatusMachine = setup({
-	types: {
-		context: {} as {
-			system_status?: Output<typeof SyncthingSystemStatus>;
-			syncthingREST: SyncthingFromREST;
-			error?: string;
-		},
-		input: {} as {
-			syncthingREST: SyncthingFromREST;
-		},
-	},
-}).createMachine({
-	id: "syncthingSystemStatus",
-	context: ({ input }) => ({
-		syncthingREST: input.syncthingREST,
-		system_status: undefined,
-	}),
-	initial: "idle",
-	states: {
-		idle: {
+		WaitUntilNextUpdate: {
 			after: {
-				1000: "fetch",
+				1000: "update syncthing",
 			}
 		},
-		"fetch": {
-			invoke: {
-				id: "fetchConfig",
-				src: fetchSystemStatus,
-				input: ({ context: { syncthingREST } }) => ({ syncthingREST }),
-				onDone: {
-					target: "success",
-					actions: assign({
-						system_status: ({ event }) => {
-							return event.output
-						}
-					})
-				},
-				onError: {
-					target: "failure",
-					actions: assign({
-						error: () => {
-							return "An error occured"
-						}
-					})
-				}
-			},
-		},
-		success: {
-			on: {
-				FETCH: "idle"
-			}
-		},
-		failure: {
-			on: {
-				RETRY: "fetch"
-			}
-		}
+		// success: {
+		// 	type: "final",
+		// },
+		// failure: {
+		// 	on: {
+		// 		RETRY: "update syncthing"
+		// 	}
+		// }
 	},
-});
-
-
-export const lightMachine = setup({}).createMachine({
-	/** @xstate-layout N4IgpgJg5mDOIC5QBsCWUAWAXAQgV2QCMA6AewDNyBiAFQHkBxBgGQFEBtABgF1FQAHUrFRZUpAHZ8QAD0QBGAGwLicgJzrVAdgBMAVm2cAHJ22aANCACe8gMydiGjdoAsczs8ObDAX28W0mLgEJBK0jCwcPFKCwqISUrIIispqGjr6RibmVoiGcsTajjY2upqcqh66vn4g4qQQcFIB2PhE0UIiYpJIMogAtAoW1ggDxJzjE5OTmr7+6C3BZJTtsV0JiM7aQ7nKqobFhZq6hoZanHKzIM1BRGTdAh1x970I2qfEmnIuqiZ2ngoVbYId4eA5aY6nMoXapAA */
-	id: 'lightBulb',
-	initial: 'off',
-	states: {
-		off: {
-			on: {
-				TOGGLE: 'on'
-			}
-		},
-		on: {
-			on: {
-				TOGGLE: 'off'
-			}
+	on: {
+		StopUpdateSyncthingEvent: {
+			actions: log("StopUpdateSyncthingEvent"),
+			target: "idle"
 		}
 	}
 });
